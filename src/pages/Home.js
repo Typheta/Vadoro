1ddbab435855da9969c7af59022cf8b44046789b
Jose <73000873+Typheta@users.noreply.github.com>
2024-04-21 23:20:20 -0400

    flip

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,13 +14,38 @@ function Home() {
         }
     };
 
+    const [flipCount, setFlipCount] = useState(0);
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    const toggleFlip = () => {
+        setIsFlipped(!isFlipped);
+        setFlipCount(flipCount + 1);
+    };
+
+    useEffect(() => {
+        if (flipCount === 7) {
+            const hero = document.querySelector(".hero");
+            hero.classList.add('spin');
+            const onAnimationEnd = () => {
+                hero.classList.remove('spin');
+            };
+            hero.addEventListener('animationend', onAnimationEnd);
+            return () => {
+                hero.removeEventListener('animationend', onAnimationEnd);
+            };
+        }
+    }, [flipCount]);
+
     return (
-        <div className="hero">
+        <div className={`hero ${isFlipped ? 'flip' : ''}`}>
             <Header />
             <div className="detel">
                 <h1>Vadoro offers software and database management services for your business.</h1>
                 <p></p>
                 <a href="#" onClick={handleColorChange}>Change Website Color</a>
+                <a href="#" onClick={toggleFlip}>Flip</a>
+                
+
             </div>
             <div className="images">
                 <img src={shape} alt="Shape" className="shape" />
